7481da8da50ea897d624b856291f1858f49125c5
HaJhin <apffksls333@naver.com>
2026-03-25 09:54:52 +0900

    input README
    day4

--- BJ/BJproject/src/BJ/Main.java
+++ BJ/BJproject/src/BJ/Main.java
@@ -1,7 +1,16 @@
 package BJ;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		if (h < 0 || h > 23) return;
+		int m = sc.nextInt();
+		if (m < 0 || m > 59) return;
+		int finish = sc.nextInt();
+		if (finish < 0 || finish > 24) return;
+		System.out.println(h + m + finish);
 	}
 }
