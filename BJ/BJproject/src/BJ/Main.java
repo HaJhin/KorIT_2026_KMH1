da63e63a9123d71100f19e1826448695ca02f68c
HaJhin <apffksls333@naver.com>
2026-03-25 10:08:22 +0900

    Change README
    day4

--- BJ/BJproject/src/BJ/Main.java
+++ BJ/BJproject/src/BJ/Main.java
@@ -6,11 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int h = sc.nextInt();
-		if (h < 0 || h > 23) return;
+		
 		int m = sc.nextInt();
-		if (m < 0 || m > 59) return;
-		int finish = sc.nextInt();
-		if (finish < 0 || finish > 24) return;
-		System.out.println(h + m + finish);
+		
+		int ct = sc.nextInt();
+		
+		int fh = h; int fm = m+ct; 
+		fh += fm/60; 
+		fm %= 60;
+		fh %= 24;
+		System.out.printf("%d %d",fh,fm);
 	}
 }
