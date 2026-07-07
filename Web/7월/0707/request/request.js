4541dea38972b16f626cdc2e2cdffb062ab389d7
HaJhin <apffksls333@naver.com>
2026-07-07 17:53:06 +0900

    0707IT
    work

---
+++ Web/7월/0707/request/request.js
@@ -0,0 +1,12 @@
+// 요소들을 등록
+
+// 문의 유형 선택상자
+let type = document.getElementById("type");
+let title = document.getElementById("title");
+let email = document.getElementById("email");
+let content = document.getElementById("content");
+let check = document.getElementById("check");
+let registBtn = document.getElementById("registBtn");
+let list = document.getElementById("list");
+
+let notice
